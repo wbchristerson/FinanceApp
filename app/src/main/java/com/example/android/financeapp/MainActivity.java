5f988d226737f87869c6b238743965b5240a2750
Will Christerson <wbchristerson@gmail.com>
2018-02-10 21:11:09 -0500

    Include some test text

--- app/src/main/java/com/example/android/financeapp/MainActivity.java
+++ app/src/main/java/com/example/android/financeapp/MainActivity.java
@@ -10,4 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /* Start Of Tests */
+    <TextView
+            android:text="Hi There"
+            android:layout_width="wrap_content"
+            android:layout_height="300dp"
+            android:backgroundColor="@android:color/green"
+            android:textColor="@android:color/black" />
+    /* End Of Tests */
 }
