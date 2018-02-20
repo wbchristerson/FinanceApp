e35afbd925df8c5273173879bb3b3fe16041be2a
Will Christerson <wbchristerson@gmail.com>
2018-02-19 20:37:15 -0500

    Add additional activity window

--- app/src/main/java/com/example/android/financeapp/MainActivity.java
+++ app/src/main/java/com/example/android/financeapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.financeapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showInformation(View view) {
+        Intent i = new Intent(this, Information.class);
+        startActivity(i);
     }
 
 
