6983d23dd9b14130289b460a49ade0d03910def4
Will Christerson <wbchristerson@gmail.com>
2018-02-20 21:15:15 -0500

    Use string values in place of literals

--- app/src/main/java/com/example/android/financeapp/MainActivity.java
+++ app/src/main/java/com/example/android/financeapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, Information.class);
         startActivity(i);
     }
+
+    Button infoButton = (Button) findViewById(R.id.info_button);
+
+    infoButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+    });
 
 
 }
